00bac802290c885d8f39679dc003ceb3e5875bf2
ahmedkhaled4d <ahmedkhaled4d@gmail.com>
2023-03-30 22:36:28 +0200

    timeoutSeconds

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,11 @@ import ADMIN_API from "./modules/admin/routes";
 import CLIENT_API from "./modules/client/routes";
 
 export const admin = functions.https.onRequest(ADMIN_API);
-export const client = functions.https.onRequest(CLIENT_API);
+export const client = functions
+  .runWith({
+    // Ensure the function has enough memory and time
+    // to process large files
+    timeoutSeconds: 300,
+    memory: "1GB"
+  })
+  .https.onRequest(CLIENT_API);
